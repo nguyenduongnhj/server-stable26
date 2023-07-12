yayo98j/server

fix share roots always being marked as writable
Signed-off-by: Robin Appelman <robin@icewind.nl>

lib/public/Files/DavUtil.php
@@ -32,6 +32,11 @@
 
 namespace OCP\Files;
 
+use OC\Files\Mount\MoveableMount;
+use OCA\Files_Sharing\SharedMount;
+use OCA\Files_Sharing\SharedStorage;
+use OCP\Constants;
+
 /**
  * This class provides different helper functions related to WebDAV protocol
  *
@@ -75,12 +80,23 @@ class DavUtil {
 		if ($info->isUpdateable()) {
 			$p .= 'NV'; // Renameable, Moveable
 		}
+
+		// since we always add update permissions for the root of movable mounts (and thus shares)
+		// we need to check the shared cache item directly to determine if it's writable
+		$storage = $info->getStorage();
+		if ($info->getInternalPath() === '' && $storage->instanceOfStorage(SharedStorage::class)) {
+			$shareEntry = $storage->getCache()->get('');
+			$isWritable = ($shareEntry->getPermissions() & Constants::PERMISSION_UPDATE);
+		} else {
+			$isWritable = $info->isUpdateable();
+		}
+
 		if ($info->getType() === FileInfo::TYPE_FILE) {
-			if ($info->isUpdateable()) {
+			if ($isWritable) {
 				$p .= 'W';
 			}
 		} else {
-			if ($info->isCreatable()) {
+			if ($isWritable) {
 				$p .= 'CK';
 			}
 		}

nguyenduongnhj/server-stable26

lib/public/Files/DavUtil.php
@@ -32,6 +32,9 @@
 
 namespace OCP\Files;
 
+use OCP\Constants;
+use OCP\Files\Mount\IMovableMount;
+
 /**
  * This class provides different helper functions related to WebDAV protocol
  *
@@ -73,10 +76,21 @@ class DavUtil {
 			$p .= 'D';
 		}
 		if ($info->isUpdateable()) {
-			$p .= 'NV'; // Renameable, Moveable
+			$p .= 'NV'; // Renameable, Movable
 		}
+
+		// since we always add update permissions for the root of movable mounts
+		// we need to check the shared cache item directly to determine if it's writable
+		$storage = $info->getStorage();
+		if ($info->getInternalPath() === '' && $info->getMountPoint() instanceof IMovableMount) {
+			$rootEntry = $storage->getCache()->get('');
+			$isWritable = $rootEntry->getPermissions() & Constants::PERMISSION_UPDATE;
+		} else {
+			$isWritable = $info->isUpdateable();
+		}
+
 		if ($info->getType() === FileInfo::TYPE_FILE) {
-			if ($info->isUpdateable()) {
+			if ($isWritable) {
 				$p .= 'W';
 			}
 		} else {